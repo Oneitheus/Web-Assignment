78aff79a0e55f1db3ebf92f2aa49883afb304ceb
Clifford <150569433+Oneitheus@users.noreply.github.com>
2024-02-29 07:25:57 -0500

    Add project files.

---
+++ Web Assignment/wwwroot/js/site.js
@@ -0,0 +1,21 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+function slide() {
+    var id = null;
+    var element = document.getElementById("animate");
+    var pos = 500;
+    console.log(pos + "%");
+    clearInterval(id)
+    id = setInterval(frame, 5);
+    function frame() {
+        if (pos == 0) {
+            clearInterval(id)
+        } else {
+            pos--;
+            element.style.marginLeft = -pos + "%";
+        }
+    }
+}
